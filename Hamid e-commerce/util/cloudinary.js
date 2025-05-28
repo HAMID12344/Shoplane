ff11684340c67d8829e7b000c4d6d906eec7e984
HAMIDALI234567 <www.hamidaligujjar.22@gmail.com>
2025-05-28 21:34:41 +0500

    asd

--- Hamid e-commerce/util/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "dcbug4o9t",
-  api_key: "328455396214445",
-  api_secret: "J7AR80MsA8ON17u_V-p_h1CGa9I",
-});
-
-module.exports = cloudinary;
